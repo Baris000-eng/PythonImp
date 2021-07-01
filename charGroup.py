963265dde480156b73c5e2c2a96c724a3d4683ed
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:44:02 +0000

    formattedStrings practice

--- charGroup.py
+++
@@ -1,7 +0,0 @@
-string='Hi, how are you, fine thanks and you'
-charGroup=string[0:3] #3(end index) is not included.
-print(charGroup)
-
-
-
-#This is a python program that gets the first three characters of a string and prints them to the terminal.
