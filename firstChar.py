e05ad886e44de508aeb24d77a4200076ef25a9ec
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:30:10 +0000

    char exercise-1

---
+++ firstChar.py
@@ -0,0 +1,7 @@
+string='Hi, how are you, fine thanks and you'
+firstChar=string[0]
+print(firstChar)
+
+
+
+#This is a python program that gets the first character of a string and prints it to terminal.
--- pound->kg.py
+++
@@ -1,7 +0,0 @@
-kgPounds=input('Weight in pounds: ');
-kgWeight=(0.45)*(float(kgPounds))
-print(kgWeight)
-
-
-
-#This is a python program that asks the weight of the user, converts the weight to kg, and prints the converted weight to terminal.
