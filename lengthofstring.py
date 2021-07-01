6bd5417fd0cc8858da7cb6bd96f972b5b975637a
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:57:15 +0000

    lengthhhh

---
+++ lengthofstring.py
@@ -0,0 +1,6 @@
+string= "Mike"
+lengthofstring= len(string)
+print(lengthofstring)
+
+
+#The python implementation which calculates the length of the string.(i.e the total number of chars in a string)
