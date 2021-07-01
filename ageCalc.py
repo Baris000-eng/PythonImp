a1a56b07b8da264a93463fd42d73b06ff3d8f9ee
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:25:40 +0000

    abc

--- ageCalc.py
+++
@@ -1,7 +0,0 @@
-year=input('Birth year:?');
-age=2020-int(year)
-print(age)
-
-
-
-#This is a python program that asks the year user born, calculates the age of the user, and prints it to the terminal.
