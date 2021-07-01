23b05625ad548f6621299de8ec8a7163f784cf23
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:54:40 +0000

    abbbcbcbcb

--- lengthStr.py
+++
@@ -1,7 +0,0 @@
-arbitraryString= "Hi, what is your name?"
-arbitraryStringLength=len(arbitraryString)
-print(arbitraryStringLength)
-#This is a python program that calculates the number of charachters in a string, and prints this number to terminal. 
-
-
-
