7bbf3a0af9f6282eec402c1c9f3145378b9b85e6
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:31:51 +0000

    char exercise-2

---
+++ lastChar.py
@@ -0,0 +1,7 @@
+string='Hi, how are you, fine thanks and you'
+lastChar=string[-1]
+print(lastChar)
+
+
+
+#This is a python program that gets the last character of a string and prints it to terminal.
