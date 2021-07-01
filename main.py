a5b9200612256345ec13acf5537421f0a5c4ce20
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-07-01 15:08:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+name= "John Smith"
+age=20;
+isNewPatient= True;
+
+
+print(name,age,isNewPatient)
+
+
+#This is a python program that prints the name-surname of the patient, the age of the patient, and whether the patient is new or not.
+
